d216fd8b2aacb06f1a095ea8a462c576ea9577fd
Instituto Despertar da Cidadania - Certifica AgriFamiliar ESG <despertar.osc@gmail.com>
2025-12-02 15:09:17 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Garante que o Tailwind analise todos os seus arquivos de código
+  ],
+  theme: {
+    extend: {
+      // Você pode adicionar cores personalizadas, fontes, etc., aqui
+    },
+  },
+  plugins: [],
+}
