2b9690a21b9482663a39a13c886fda7655a97820
Instituto Despertar da Cidadania - Certifica AgriFamiliar ESG <despertar.osc@gmail.com>
2025-12-03 13:09:06 -0300

    Update main.tsx

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AuthProvider } from "../contexts/AuthContext";
-// CORREÇÃO TS2307: Adicionando a extensão .tsx
-import App from "./App.tsx"; 
-import "../index.css";
+import { AuthProvider } from "./contexts/AuthContext";
+import App from "./App.tsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
